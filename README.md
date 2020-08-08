57a679ddaf036d7eff49a1737677a9f08ef64f31
lok660 <y19971@126.com>
2020-08-09 01:08:40 +0800

    doc:增加readme

---
+++ README.md
@@ -0,0 +1,171 @@
+```js
+//开发环境使用
+devtool:'souce-map' //错误检测
+//babel 缓存
+cacheDirectory:true
+//devserver:
+devServer{
+//gzip 压缩
+compress:true,
+prot:3000,
+hot:true,
+//不显示日志
+clientLogLevel:'silent',
+//只打印基本信息
+quiet:true,
+//代理 跨域
+proxy:{
+'/api':{
+target:'localhost:3000/api',
+//发送请求路径重写
+pathRewrite:{}
+},
+//忽略
+watchOptions:{
+ignored:/node_modules/
+}
+}
+//生产环境
+//文件缓存
+
+[hash]:index.hash.js
+
+[chunkhash]:长效缓存 ,如果打包来源相同 chunk hash 相同
+[contenthash]:不同文件使用不同的 hash 比如 jscss 不同
+```
+
+# tree shaking
+
+树摇:去除代码没有使用的代码
+
+```js
+必须 es6 模块化 生产环境
+package.json
+"sideEffects":["*.css"] #抽离 node_module
+omtimization:{
+splitChunks:{
+chunks:'all'
+}
+}
+```
+
+# 懒加载 lazy-loadding
+
+```js
+预加载 webpackPrefetch:true //空闲提前加载
+import(/_ webpackChunkName: "print",webpackPrefetch:true_/'./').then(()=>{})
+```
+
+# 多进程打包
+
+```js
+thread-loader
+哪个 loader 用就配置哪个 常用于 babel
+常用于大量代码
+如:
+use[
+{
+loader:'thread-loader',
+options:{workers:2}//设置进程
+},
+{...loader}
+]
+```
+
+## externals
+
+```js
+使用 cdn 代替
+//webpack.config.js
+externals:{
+库名:npm 包名
+Jquery:"jQuery"
+}
+然后在 html 引入 cdn
+```
+
+## dll
+
+动态连接库 将多个库打包为一个 chunk
+
+## resolve
+
+```js
+webpack.config.js
+resolve:{
+//配置路径别名
+alias:{
+$css:resolve(__dirname,'src/css/index.css')
+    //import $css === import '...index.css'
+}
+//忽略文件名
+extensions:['.js','.css']
+告诉 webpack 哪里查找 node_modules
+modules:[resolve(__dirname,'../../node_modules'),node_modules]
+}
+```
+
+## optimization
+
+```js
+
+optimization:{
+  splitChunks:{
+    chunks:'all',
+    minSize:30*1024, //分割代码大于30kb 小于不分割
+    maxSize:0//无最大限制
+    minChunks:1,至少引用一起才会被chunk
+    maxAsyncRequests:5 //按需加载时并行加载的数量
+    maxInitialRequests:3 //入口js最大并行请求数量
+    automaticNameDelimiter:'~'//名称连接符号,xx~
+    name:true //命名规则
+    cacheGroups:{
+      //分割规则
+      vendors:{
+        test:/node_modules/,//需要写成正则
+          priority:-10
+      }
+    },
+      //js压缩
+     minimizer: [new TerserPlugin(
+     cache:true,
+     parallel:true
+     )],
+}
+```
+
+## code
+
+```js
+ optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({}),
+    new terserwebpackplugin({
+      cache: true,
+      parallel: true
+    })],
+    runtimeChunk: {
+      name: 'manifest',
+    },
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: false,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+        styles: {
+          name: 'styles',
+          test: /\.(scss|css)$/,
+          chunks: 'all',
+          enforce: true
+        }
+      },
+    },
+  },
+```
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,83 +8,96 @@ const prodConfig = require('./webpack.prod.js');
 const merge = require('webpack-merge');
 
 const commonConfig = {
-	entry: {
-		main: "./src/index.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							plugins: ["@babel/plugin-transform-runtime"]
-						}
-					}
-				]
-			}, {
-				test: /\.(png|jpg|gif)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[name]_[hash].[ext]',
-						outputPath: 'images/',
-					}
-				}
-			}, {
-				test: /\.(eot|ttf|svg|woff|woff2)$/,
-				use: {
-					loader: 'file-loader',
-				}
-			}]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		// new BundleAnalyzerPlugin({
-		// 	analyzerHost: '127.0.0.1',
-		// 	analyzerPort: 8889,
-		// 	openAnalyzer: false,
-		// }),
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			_: 'lodash',
-			_join: ['lodash', 'join']
-		})
-	],
-	optimization: {
-		usedExports: true,
-		runtimeChunk: {
-			name: 'runtime',
-		},
-		splitChunks: {
-			chunks: 'all',
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10,
-					name: 'vendors',
-				}
-			}
-		},
-	},
-	performance: false, // 关闭性能上的一些问题
+  entry: {
+    main: "./src/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: ["@babel/plugin-transform-runtime"]
+            }
+          }
+        ]
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/',
+          }
+        }
+      }, {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+        }
+      }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    // new BundleAnalyzerPlugin({
+    // 	analyzerHost: '127.0.0.1',
+    // 	analyzerPort: 8889,
+    // 	openAnalyzer: false,
+    // }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'lodash',
+      _join: ['lodash', 'join']
+    })
+  ],
+  optimization: {
+    usedExports: true,
+    runtimeChunk: {
+      name: 'runtime',
+    },
+    //  代码分隔
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        //  第三方依赖
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: 1,  //  设置优先级,首先抽离第三方模块
+          name: 'vendors',
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 1 //  最少引入了1次
+        },
+        //  缓存组
+        common: {
+          //  公共模块
+          chunks: 'initial',
+          name: 'common',
+          minSize: 100,  //  大小超过100个字节
+          minChunks: 3,  //  最少引入了3次
+        }
+      }
+    },
+  },
+  performance: false, // 关闭性能上的一些问题
 }
 
 module.exports = env => {
-	console.log('env', env);
-	if (env) {
-		return merge(commonConfig, prodConfig);
-	} else {
-		return merge(commonConfig, devConfig);
-	}
+  console.log('env', env);
+  if (env) {
+    return merge(commonConfig, prodConfig);
+  } else {
+    return merge(commonConfig, devConfig);
+  }
 };
